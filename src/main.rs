66758eab78ecb1813e3f81e016b1b3399a597b9d
skps2010 <skps2010030908@yahoo.com>
2024-01-27 17:22:22 +0800

    add timer and sound

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 mod sound_player;
 
-use bevy::{input::{keyboard::KeyboardInput, ButtonState}, prelude::*};
+use bevy::{
+    input::{keyboard::KeyboardInput, ButtonState},
+    prelude::*,
+};
 use sound_player::*;
+
+#[derive(Resource)]
+struct StepSound(Handle<AudioSource>);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, phah)
+        .add_systems(Update, sound_timer)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    let mut sound_player: SoundPlayer = SoundPlayer::new(5000);
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut sound_player: SoundPlayer = SoundPlayer::new(1000);
     sound_player.add_action(Action::new(vec![1, 2, 2], ActionType::Attack));
-    sound_player.start();
 
     commands.insert_resource(sound_player);
+
+    let ball_collision_sound = asset_server.load("sounds/gong.ogg");
+    commands.insert_resource(StepSound(ball_collision_sound));
 }
 
 fn phah(mut events: EventReader<KeyboardInput>, mut sound_player: ResMut<SoundPlayer>) {
@@ -29,8 +38,26 @@ fn phah(mut events: EventReader<KeyboardInput>, mut sound_player: ResMut<SoundPl
                 Some(KeyCode::S) => {
                     sound_player.key_down(2);
                 }
+                Some(KeyCode::D) => {
+                    println!("start");
+                    sound_player.start();
+                }
                 _ => continue,
             }
         }
+    }
+}
+
+fn sound_timer(
+    mut sound_player: ResMut<SoundPlayer>,
+    mut commands: Commands,
+    sound: Res<StepSound>,
+) {
+    if sound_player.update() {
+        commands.spawn(AudioBundle {
+            source: sound.0.clone(),
+            // auto-despawn the entity when playback finishes
+            settings: PlaybackSettings::DESPAWN,
+        });
     }
 }
